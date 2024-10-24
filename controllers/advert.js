8a615417d00206e9f946aca40b752509f446b71c
Perp-pexel <perpaman44@gmail.com>
2024-10-24 19:18:25 +0000

    limts amend

--- controllers/advert.js
+++ controllers/advert.js
@@ -22,7 +22,7 @@ export const addAdvert = async (req, res, next) => {
 
 export const getAdverts = async (req, res, next) => {
     try {
-        const { filter = "{}", sort = "{}", limit = 1000, skip = 0 } = req.query;
+        const { filter = "{}", sort = "{}", limit = 20, skip = 0 } = req.query;
 
         const adverts = await AdvertModel
             .find(JSON.parse(filter))
