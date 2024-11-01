4ef2fc149fc680c67a602bb7ec35f3484e72243d
Perp-pexel <perpaman44@gmail.com>
2024-11-01 22:55:04 +0000

    mail

--- utils/mail.js
+++ utils/mail.js
@@ -5,8 +5,8 @@ export const mailTransporter = createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: 'perpaman44@gmail.com',
-        pass: 'grfhgzkyxmvogatz'
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     },
     from: 'perpaman44@gmail.com'
 });
